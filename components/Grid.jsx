e891b04ac378c342dedb696076c3d4d3a7c3be71
ana.herbel <anamariaherbel@gmail.com>
2025-04-07 08:51:22 +0300

    create and style main page, header, footer

---
+++ components/Grid.jsx
@@ -0,0 +1,8 @@
+//this will be the recipe card grid
+import React from 'react'
+
+export default function Grid() {
+  return (
+    <div>Grid</div>
+  )
+}
---
+++ components/RecipeCard.jsx
@@ -0,0 +1,8 @@
+//this will contain one recipe
+import React from 'react'
+
+export default function RecipeCard() {
+  return (
+    <div>RecipeCard</div>
+  )
+}
