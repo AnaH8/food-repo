2b7273126ca220af5c8ecead996f69bed4a58949
ana.herbel <anamariaherbel@gmail.com>
2025-04-09 21:34:39 +0300

    style modal

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,13 +14,12 @@ export default function Header() {
 
   return (
     <header>
-
       <div className='p-2 h-[10vh] sm:h-fit sm:p-4 md:p-6 flex items-center justify-between gap-4 bg-[#d9c5b3] sticky z-1000 top-0 left-0
     text-lg sm:text-xl md:text-4xl text-white '>
-        <Link href={'/'}>
+        <Link href={'/'} onClick={()=> setToggleSearch(false)}>
         <i className="fa-solid fa-bowl-food  pl-[0.5rem]"> F P</i>
         </Link>
-        <button onClick={handleToggle}>
+        <button onClick={()=> setToggleSearch(true)}>
           <i className="fa-solid fa-magnifying-glass pr-[1rem]"></i>
         </button>
       </div>
--- components/Search.jsx
+++ components/Search.jsx
@@ -1,11 +1,24 @@
 'use client'
+import { Nunito_Sans } from "next/font/google";
 import React from "react";
+
+const nunito = Nunito_Sans({subsets: ["latin"], weight: ["400"]})
 
 export default function Search(props) {
   const {handleToggle} = props
 
   return (
-  <div className="z-100" onClick={handleToggle}>Modal
+  <div className="absolute w-screen h-screen bg-white z-1000 flex flex-col gap-14" >
+    <button className="p-2 text-4xl text-black" onClick={handleToggle}>
+    <i className="fa-solid fa-x text-[#2b4257]"></i>
+    </button>
+    <form className={"flex justify-around items-center p-2 md:w-fit self-center " + nunito.className }>
+      <input className="bg-gray-100 p-4 rounded-s-4xl w-[100%] md:w-fit" type="text" placeholder="search recipes"/>
+      <button className="bg-[#d17a55] p-4 rounded-e-4xl text-white text-sm ">
+        SEARCH
+      </button>
+    </form>
+
   </div>
 )
 }
